8aa2c00e9ee0daa969132f2b28ff21eb064d15c3
Arpit Bansal <abansal3@babson.edu>
2016-07-04 17:41:36 +0530

    Setup basic console

--- js/main.js
+++ js/main.js
@@ -1,17 +1,27 @@
 $(function () {
-  var jqconsole = $('#console').jqconsole("\n\n\n\n\n\n\n\n");
+  var jqconsole = $('#console').jqconsole("");
 
-  function process(input){
+  function process(input) {
     var parsed = input.split(" ");
-    switch(parsed[0]){
-      case "?":
-      case "ls":
-      case "help":
-        return text.help();
-      case "clear":
-        return text.clear();
-      default:
-        return text.notRight;
+    switch(parsed[0].toLowerCase()){
+        case "?":
+            return text.help();
+        case "help":
+            return text.help();
+        case "info":
+            return text.info;
+        case "contact":
+            return text.contact;
+        case "projects":
+            return text.showProjects();
+        case "members":
+            return text.showMembers();
+        case "events":
+            return text.showEvents();
+        case "clear":
+            return text.clear();
+        default:
+            return text.notRight;
     }
   }
 
--- js/text.js
+++ js/text.js
@@ -6,13 +6,13 @@ var text = {
 			case 1:
 				return "What's up";
 			case 2:
-				return "Nice to meet you!"
+				return "Nice to meet you!";
 			case 3:
-				return "Hello,"
+				return "Hello,";
 		}
 	},
 	help: function() {
-		return this.helpGreeting() + "\n\n"
+		return "\n" + this.helpGreeting() + "\n\n"
 		+ "Try one of the following commands:\n\n"
 		+ "\tinfo: Information about us\n\n"
 		+ "\tcontact: Email us\n\n"
@@ -20,6 +20,73 @@ var text = {
 		+ "\tmembers: Get to know the community\n\n"
 		+ "\tevents: Calendar\n\n"
 		+ "\tclear: Clears the console\n\n"
+	},
+	info: "\nXYZ\n\n",
+	contact: "\nReach out to us at <a href='https://mail.google.com/mail/u/0/?view=cm&fs=1&tf=1&to=babsoncode@gmail.com' target='_blank'>babsoncode@gmail.com</a>\n\n",
+	projects: [
+		{
+			id: 1,
+			name: "Example 1",
+			description: "Description 1",
+			url: "www.google.com",
+			status: "Ongoing"
+		},
+		{
+			id: 2,
+			name: "Example 2",
+			description: "Description 2",
+			url: "www.facebook.com",
+			status: "Completed"
+		}
+	],
+	showProjects: function () {
+		var projectOutput = "\nProjects:\n\n";
+
+		this.projects.forEach(function (project) {
+			projectOutput += ("\t"
+			+ project.id + ". " + project.name
+			+ "\n\t   "
+			+ project.description
+			+ "\n\t   "
+			+ "<a href='" + project.url + "'>" + project.url + "</a>"
+			+ "\n\t   "
+			+ "Status: " + project.status
+			+ "\n\n");
+		});
+
+		return projectOutput;
+	},
+	members: [
+		{
+			name: "Arpit Bansal",
+			age: 18,
+			email: "abansal3@babson.edu",
+			linkedIn: "www.linkedin.com",
+			skills: "Web Dev"
+		},
+		{
+			name: "Waseem Shabout",
+			age: 19,
+			email: "wshabout1@babson.edu",
+			linkedIn: "www.linkedin.com",
+			skills: "UX/UI Design"
+		}
+	],
+	showMembers: function () {
+		var memberOutput = "\nMembers:\n\n";
+
+		this.members.forEach(function (member) {
+			memberOutput += ("\t"
+			+ member.name + ", " + member.age
+			+ "\n\t" + "<a href='https://mail.google.com/mail/u/0/?view=cm&fs=1&tf=1&to=" + member.email + "' target='_blank'>" + member.email + "</a>"
+			+ "\n\t" + member.linkedIn
+			+ "\n\n");
+		});
+
+		return memberOutput;
+	},
+	showEvents: function () {
+		return "\nCalendar api has not been set up yet\n\n"; //API Call
 	},
 	notRight: "Oops, that's not a valid command... try '?'\n",
 	clear: function() {
